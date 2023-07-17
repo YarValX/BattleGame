bf495c4a52b171a50b50334371d1c4d606c9a29a
YarValX <124351076+YarValX@users.noreply.github.com>
2023-07-17 10:57:00 +0500

    redact

--- Program.java
+++ Program.java
@@ -1,4 +1,5 @@
 import units.*;
+
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
@@ -18,36 +19,20 @@ public class Program {
         for (int count = 1; count < 11; count++) {
             int newCharacter = new Random().nextInt(0, 4);
             switch (newCharacter) {
-                case 0 -> {
-                    alliance.add(new Mage(getName(), 1, count));
-                }
-                case 1 -> {
-                    alliance.add(new Spearman(getName(), 1, count));
-                }
-                case 2 -> {
-                    alliance.add(new Sniper(getName(), 1, count));
-                }
-                default -> {
-                    alliance.add(new Peasant(getName(), 1, count));
-                }
+                case 0 -> alliance.add(new Mage(getName(), 1, count));
+                case 1 -> alliance.add(new Spearman(getName(), 1, count));
+                case 2 -> alliance.add(new Sniper(getName(), 1, count));
+                default -> alliance.add(new Peasant(getName(), 1, count));
             }
         }
 
         for (int count = 1; count < 11; count++) {
             int newCharacter = new Random().nextInt(0, 4);
             switch (newCharacter) {
-                case 0 -> {
-                    empire.add(new Monk(getName(), 10, count));
-                }
-                case 1 -> {
-                    empire.add(new Outlaw(getName(), 10, count));
-                }
-                case 2 -> {
-                    empire.add(new Crossbowman(getName(), 10, count));
-                }
-                default -> {
-                    empire.add(new Peasant(getName(), 10, count));
-                }
+                case 0 -> empire.add(new Monk(getName(), 10, count));
+                case 1 -> empire.add(new Outlaw(getName(), 10, count));
+                case 2 -> empire.add(new Crossbowman(getName(), 10, count));
+                default -> empire.add(new Peasant(getName(), 10, count));
             }
         }
 
